fce3a7b86b4b8377ac1679c7f5e135d0a15cb0d7
ammarkhokhar007 <ammarsami734@gmail.com>
2024-01-04 16:33:27 +0500

    error resolve

--- src/components/common/Topnav.jsx
+++ src/components/common/Topnav.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Topnav() {
- 
+  const handleToggleClick = () => {
+    let navigation = document.querySelector(".navigation");
+    let main = document.querySelector(".main");
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+  };
+
   return (
     <div>
-        <div className="topbar">
-  <div className="toggle">
-    <ion-icon name="menu-outline"/>
-  </div>
-  <div className="search">
-    <label>
-      <input type="text" placeholder="Search here" />
-      <ion-icon name="search-outline" />
-    </label>
-  </div>
-  <div className="user">
-    <img src="./assets/imgs/customer01.jpg" alt />
-  </div>
-</div>
-
-
-
+      <div className="topbar">
+        <div className="toggle" onClick={handleToggleClick}>
+          <ion-icon name="menu-outline" />
+        </div>
+        <div className="search">
+          <label>
+            <input type="text" placeholder="Search here" />
+            <ion-icon name="search-outline" />
+          </label>
+        </div>
+        <div className="user">
+          <img src="./assets/imgs/customer01.jpg" alt />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Topnav
+export default Topnav;
